ce8413a95f68fa261436a058d7143ec86ca82a90
Dac Vien Le <53806253+ledacvien@users.noreply.github.com>
2024-04-12 20:51:54 -0700

    Sync call API

--- app/src/main/java/com/example/threatawarenessmobile/MainActivity.kt
+++ app/src/main/java/com/example/threatawarenessmobile/MainActivity.kt
@@ -13,25 +13,32 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.awaitResponse
 
 class MainActivity : AppCompatActivity() {
 
     private val handler = Handler(Looper.getMainLooper())
+    private var callingAPI: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnSafe : Button = findViewById(R.id.btnSafe)
-        btnSafe.setOnClickListener {
+        val btnStop : Button = findViewById(R.id.btnStop)
+        btnStop.setOnClickListener {
             clickSafeButton()
         }
 
         val btnActivate : Button = findViewById(R.id.btnActivate)
         btnActivate.setOnClickListener {
             Toast.makeText(this, "Start checking traffic", Toast.LENGTH_SHORT).show()
-            clickActivateButton()
+            callingAPI = clickActivateButton()
         }
     }
 
@@ -39,11 +46,43 @@ class MainActivity : AppCompatActivity() {
         val circle : ImageView = findViewById(R.id.imageView)
         val textView : TextView = findViewById(R.id.textView)
         circle.setImageResource(R.drawable.green_circle)
-        textView.text = "SAFE"
+        textView.text = "App Stop"
+        callingAPI?.cancel()
         vibratePhone(200)
     }
 
-    private fun clickActivateButton(){
+    private fun clickActivateButton(): Job{
+        return GlobalScope.launch(Dispatchers.Main) {
+            Toast.makeText(this@MainActivity,"API working", Toast.LENGTH_SHORT).show()
+            while (true) {
+                val response = dataService.getWarning().awaitResponse()
+
+                if (response.isSuccessful) {
+                    val data = response.body()
+                    val textView : TextView = findViewById(R.id.textView)
+                    val circle : ImageView = findViewById(R.id.imageView)
+
+                    if (data?.safe_to_cross == true) {
+                        circle.setImageResource(R.drawable.green_circle)
+                        textView.textSize = 20f
+                        textView.text = "PROCEED WITH CAUTION"
+                        delay(20) // Delay for 20 milliseconds if the return value is true
+                    } else {
+                        circle.setImageResource(R.drawable.red_circle)
+                        textView.textSize = 40f
+                        textView.text = "DANGER!!"
+                        vibratePhone(500)
+                        delay(5000) // Delay for 2 seconds if the return value is false
+                    }
+                } else {
+                    // Handle unsuccessful response
+                }
+            }
+        }
+    }
+
+    private fun handlerPost()
+    {
         handler.post(object : Runnable{
             override fun run() {
                 handler.postDelayed(this, fetchDataFromAPI())
